3d4b39e8486542326f2edf6c93402cddd97b1c5b
usamaks <108808328+usamaks@users.noreply.github.com>
2023-03-11 13:01:55 +0530

    Add files via upload

---
+++ TestData/persondetails.sql
@@ -0,0 +1,6 @@
+insert into mydb.person values(1,"jonah");
+insert into mydb.person values(2,"joseph");
+insert into mydb.person values(3,"job");
+insert into mydb.person values(4,"solomon");
+insert into mydb.person values(4,"lut");
+
